62b9f598790d15be3764e75a4ec2236282b92eb0
Mehadi Hasan <Mehadihasan.phy@gmail.com>
2022-05-22 23:01:37 +0600

    Project setup and Navbar added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Navbar from './Pages/Shared/Navbar';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Products from './Pages/Products/Products';
+import Blog from './Pages/Blog/Blog';
+import Portfolio from './Pages/Portfolio/Portfolio';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path='products' element={<Products></Products>}></Route>
+      <Route path='blog' element={<Blog></Blog>}></Route>
+      <Route path='portfolio' element={<Portfolio></Portfolio>}></Route>
+      <Route path='login' element={<Login></Login>}></Route>
+      <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
