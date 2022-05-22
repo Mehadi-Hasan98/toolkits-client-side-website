1d4f36f4240b3bf6e6670e54e8e2043f8c9eb48b
Mehadi Hasan <Mehadihasan.phy@gmail.com>
2022-05-22 23:15:28 +0600

    Footer added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Footer from '../Shared/Footer';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
-            <h2>This is Home</h2>
+            <Banner></Banner>
+            <Footer></Footer>
         </div>
     );
 };
